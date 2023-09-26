2249eb88dd321455714966fc91a2b0bbce825478
elisei4ou <elisei.29@abv.bg>
2023-09-26 13:57:36 +0300

    Updater my first project

---
+++ rock_paper_scissors.py
@@ -0,0 +1,35 @@
+import random
+
+rock = 'Rock'
+paper = 'Paper'
+scissors = 'Scissors'
+player_move = input("Choose: [r]ock, [p]aper or [s]cissors: ")
+
+if player_move == "r":
+    player_move = "rock"
+elif player_move == "p":
+    player_move = "paper"
+elif player_move == "s":
+    player_move = "scissors"
+else:
+    raise SystemExit("Invalid input. Try again...")
+
+computer_random_move = random.randint(1, 3)
+computer_move = ""
+if computer_random_move == 1:
+    computer_move = "rock"
+elif computer_random_move == 2:
+    computer_move = "paper"
+else:
+    computer_move = "scissors"
+print(f"The computer chose {computer_move}.")
+
+if (player_move == "rock" and computer_move == "scissors")\
+        or (player_move == "paper" and computer_move == "rock")\
+        or (player_move == "scissors" and computer_move == "paper"):
+    print("You WIN")
+elif computer_move == player_move:
+    print("Draw")
+else:
+    print("You LOSE")
+
